817b297182f846440426137f203807904ed781ea
Roman Sachse <r.sachse@a-s-d.de>
2018-03-21 09:23:08 +0100

    Make it finally work
    Remove bundle.js because it is injected.
    
    Change div id to make css work again
    
    Remove CaseSensitivePlugin because Windows/Fable

--- config/webpack.config.fable.js
+++ config/webpack.config.fable.js
@@ -1,13 +1,14 @@
+const autoprefixer = require("autoprefixer");
 const path = require("path");
 const webpack = require("webpack");
-const autoprefixer = require("autoprefixer");
-const eslintFormatter = require("react-dev-utils/eslintFormatter");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+// const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin"); see commment in plugins
 const InterpolateHtmlPlugin = require("react-dev-utils/InterpolateHtmlPlugin");
 const WatchMissingNodeModulesPlugin = require("react-dev-utils/WatchMissingNodeModulesPlugin");
-const paths = require("./paths");
+const eslintFormatter = require("react-dev-utils/eslintFormatter");
+// const ModuleScopePlugin = require("react-dev-utils/ModuleScopePlugin"); see commment in resolve.plugins
 const getClientEnvironment = require("./env");
+const paths = require("./paths");
 const fableUtils = require("fable-utils");
 
 // Webpack uses `publicPath` to determine where the app is being served from.
@@ -55,14 +56,43 @@ module.exports = {
   output: {
     // Add /* filename */ comments to generated require()s in the output.
     pathinfo: true,
-    filename: "bundle.js",
-    path: resolve("../public"),
+    // This does not produce a real file. It's just the virtual path that is
+    // served by WebpackDevServer in development. This is the JS bundle
+    // containing code from all our entry points, and the Webpack runtime.
+    filename: "static/js/bundle.js",
+    // There are also additional JS chunk files if you use code splitting.
+    chunkFilename: "static/js/[name].chunk.js",
+    publicPath: publicPath,
     // Point sourcemap entries to original disk location (format as URL on Windows)
     devtoolModuleFilenameTemplate: info =>
       path.resolve(info.absoluteResourcePath).replace(/\\/g, "/")
   },
   resolve: {
-    modules: [resolve("../node_modules/")]
+    modules: [resolve("../node_modules/"), paths.appNodeModules].concat(
+      // It is guaranteed to exist because we tweak it in `env.js`
+      process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
+    ),
+    // These are the reasonable defaults supported by the Node ecosystem.
+    // We also include JSX as a common component filename extension to support
+    // some tools, although we do not recommend using it, see:
+    // https://github.com/facebookincubator/create-react-app/issues/290
+    // `web` extension prefixes have been added for better support
+    // for React Native Web.
+    extensions: [".web.js", ".mjs", ".js", ".json", ".web.jsx", ".jsx"],
+    alias: {
+      // Support React Native Web
+      // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
+      "react-native": "react-native-web"
+    },
+    plugins: [
+      // Prevents users from importing files from outside of src/ (or node_modules/).
+      // This often causes confusion because we only process files within src/ with babel.
+      // To fix this, we prevent you from importing files out of src/ -- if you'd like to,
+      // please link the files into your node_modules/ and let module-resolution kick in.
+      // Make sure your source files are compiled, as they will not be processed in any way.
+      // Fable needs to import files outside of src/ (or node_modules/)
+      // new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson])
+    ]
   },
   module: {
     strictExportPresence: true,
@@ -113,14 +143,6 @@ module.exports = {
               }
             }
           },
-          // {
-          //   test: /\.js$/,
-          //   exclude: /node_modules/,
-          //   use: {
-          //     loader: "babel-loader",
-          //     options: babelOptions
-          //   }
-          // },
           // Process JS with Babel.
           {
             test: /\.(js|jsx|mjs)$/,
@@ -213,7 +235,8 @@ module.exports = {
     // Watcher doesn't work well if you mistype casing in a path so we use
     // a plugin that prints an error when you attempt to do this.
     // See https://github.com/facebookincubator/create-react-app/issues/240
-    new CaseSensitivePathsPlugin(),
+    // This does not work well with Fable on Windows, when the username has an uppercase letter
+    // new CaseSensitivePathsPlugin(),
     // If you require a missing module and then `npm install` it, you still have
     // to restart the development server for Webpack to discover it. This plugin
     // makes the discovery automatic so you don't have to restart.
