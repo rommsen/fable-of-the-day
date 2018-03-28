8eaecdec7b7973c7ba9ccf3ef847272bd14d2840
Roman Sachse <r.sachse@a-s-d.de>
2018-03-28 09:06:38 +0200

    Orders are stored in LocalStorage

--- src/fable/App.fs
+++ src/fable/App.fs
@@ -5,6 +5,7 @@ open Fable.Import
 open Fable.Helpers.React
 open Fable.Import.React
 open Fable.Helpers.React.Props
+open Fable.PowerPack
 open Helpers
 open Order
 open Fish
@@ -32,31 +33,27 @@ type [<Pojo>] AppState =
     Orders : Orders
   }
 
-let sampleFishes : (string * Fish) list =
-  [
-    ("fish1", {
-        name = "Pacific Halibut"
-        image = "/images/hali.jpg"
-        desc = "Everyones favorite white fish. We will cut it to the size you need and ship it."
-        price = 1724.
-        status = "available"
-      })
+type [<Pojo>] Params =
+  {
+    storeId : string
+  }
 
-    ("fish2", {
-        name = "Lobster"
-        image = "/images/lobster.jpg"
-        desc = "These tender mouth-watering beauties are a fantastic hit at any dinner party."
-        price = 3200.
-        status = "available"
-      })
-  ]
+type [<Pojo>] Match =
+  {
+    ``params`` : Params
+  }
+
+type [<Pojo>] AppProps =
+  {
+    ``match`` : Match
+  }
 
 let order props =
   ofType<Order,OrderProps,_> props []
 
 
 type App(props) as this=
-  inherit React.Component<obj,AppState>(props)
+  inherit React.Component<AppProps,AppState>(props)
   do
     this.setInitState { Fishes = Map.empty ; Orders = Map.empty }
 
@@ -108,6 +105,18 @@ type App(props) as this=
 
     this.setState { this.state with Fishes = this.state.Fishes |> Map.add key fish }
 
+
+  override __.componentDidMount () =
+    // load the orders from the localStorage and set the state when found
+    this.props.``match``.``params``.storeId
+    |> BrowserLocalStorage.load<Orders>
+    |> Option.iter (fun orders -> this.setState { this.state with Orders = orders })
+
+  override __.componentDidUpdate (_,_) =
+    // save the orders to the local storage
+    this.state.Orders
+    |> BrowserLocalStorage.save<Orders> this.props.``match``.``params``.storeId
+
   override this.render () =
     let inventoryProps : InventoryProps =
       {
@@ -119,6 +128,7 @@ type App(props) as this=
         LoadSampleFishes = loadSampleFishes
 
       }
+
     div [ ClassName "catch-of-the-day" ]
       [
         div [ ClassName "menu" ]
